09a6cb773796d29d6b4913dcdc8a5b8b7ebd462f
ichurs <chursinov_ivan@mail.ru>
2022-09-02 17:02:55 +0300

    JDBC added. Conection created

--- src/main/java/package1/Main.java
+++ src/main/java/package1/Main.java
@@ -1,7 +1,27 @@
 package package1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
+    private static final String URL = "jdbc:mysql://localhost:3306/fwstudydb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("DB connection established!");
+            }
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("DB connection closed!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed load driver class!");
+        }
     }
 }
